c8de59451c9ce676483e056082f954089d5f14fc
rafi542 <mdamirhussainrafi@gmail.com>
2026-07-30 10:15:46 +0600

    Add files via upload

---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,17 @@
+import Footer from "../Components/Footer";
+import Navigation from "../Components/Navigation";
+import { Outlet } from 'react-router';
+
+const Root = () => {
+    return (
+        <div>
+            <Navigation/>
+            
+            <Outlet/>
+
+            <Footer/>
+        </div>
+    );
+};
+
+export default Root;
