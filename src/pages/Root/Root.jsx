e52952905c9effe0b5c48bfabba011c018ddfac1
rafi542 <mdamirhussainrafi@gmail.com>
2026-07-30 10:55:58 +0600

    Update Root.jsx

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,5 +1,5 @@
-import Footer from "../Components/Footer";
-import Navigation from "../Components/Navigation";
+import Footer from "../../Components/Footer";
+import Navigation from "../../Components/Navigation";
 import { Outlet } from 'react-router';
 
 const Root = () => {
